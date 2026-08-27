94e37ee9005bcfaf8cd92d5fdc461bcf65aa6e2e
nelfonck <nelfonck@gmail.com>
2026-08-27 06:58:04 -0600

    force compatibity to android 6.0 almost yet

--- Desktop/flutter/proformas/android/app/build.gradle.kts
+++ Desktop/flutter/proformas/android/app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
         applicationId = "com.nelfonck.utilities"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -49,15 +49,17 @@ android {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = true
+            isShrinkResources = true
+
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
 }
 
-kotlin {
-    compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
-    }
-}
 
 flutter {
     source = "../.."
--- Desktop/flutter/proformas/macos/Flutter/GeneratedPluginRegistrant.swift
+++ Desktop/flutter/proformas/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,6 +9,7 @@ import file_selector_macos
 import mobile_scanner
 import open_file_mac
 import package_info_plus
+import path_provider_foundation
 import rive_native
 import share_plus
 import shared_preferences_foundation
@@ -19,6 +20,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   OpenFilePlugin.register(with: registry.registrar(forPlugin: "OpenFilePlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   RiveNativePlugin.register(with: registry.registrar(forPlugin: "RiveNativePlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
